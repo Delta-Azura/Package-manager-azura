0ca90b013e35640c53eb30a93b8376b76cdd2c53
Delta <77167689+Delta-Azura@users.noreply.github.com>
2021-04-17 17:53:21 +0200

    ajout des prérequis

--- base-install.py
+++ base-install.py
@@ -1,25 +1,38 @@
 #! /usr/bin/env python3
 
+# Les imports 
 import os 
 
-def main():
-    print("Nous syncronisons les dépôts de votre os")
-    os.system("sudo cards sync && sudo cards upgrade")
-    install =input("Quels paquets souhaitez vous installer ? : ")
-    os.system("sudo cards install " + install )
-    flatpak =input("Souhaitez vous installer les flatpaks ? : ")
-    if flatpak == yes :
-        get_flatpak =input("Quels paquets voulez vous installer ? : ")
-        try :
-            os.system("flatpak install " + get_flatpak )
-        except :
-            os.system("sudo cards install flatpak")
-            os.system("flatpak install " + get_flatpak ) 
-    if flatpak != yes : 
-        print("Nous nettoyons les archives binaires")
-        os.system("sudo cards purge")
-        print("Done")
+# Les variables
+yes = "yes"
 
+def main():
+    print("Félicitations, vous venez d'installer la distribution GNU/LINUX NuTyX !! ")
+    print("Avant d'aller plus loin, veuillez svp lire le prérequi : Tout d'abord veuillez vérifier que votre ordinateur est connecté à internet")
+    yes_or_no = input("Alors ? [yes ou no ] : ")
+    if yes == yes_or_no :
+        print("Nous syncronisons les dépôts de votre os")
+        os.system("sudo cards sync && sudo cards upgrade")
+        install =input("Quels paquets souhaitez vous installer ? : ")
+        os.system("sudo cards install " + install )
+        flatpak =input("Souhaitez vous installer les flatpaks ? : ")
+        if flatpak == yes :
+            get_flatpak =input("Quels paquets voulez vous installer ? : ")
+            try :
+                os.system("flatpak install " + get_flatpak )
+            except :
+                os.system("sudo cards install flatpak")
+                os.system("flatpak install " + get_flatpak )
+        
+        if flatpak != yes : 
+            print("Nous nettoyons les archives binaires")
+            os.system("sudo cards purge")
+            print("Done")
+      
+    if yes != yes_or_no :
+        exit()
+        
+        
 if __name__ == "__main__":
     main()
 
