74d0e0d394330831bae5c43467c8d03f3a6a73b4
Delta-Azura <nathalees@hotmail.fr>
2021-04-26 15:09:11 +0200

    correction de bug

--- azura.sh
+++ azura.sh
@@ -1,83 +1,104 @@
 #! /bin/bash
 
 # Azura in bash :
+function lock () {
+	if [[ -d /tmp/lock/azura/ ]];then
+		mkdir /tmp/lock/
+		mkdir /tmp/lock/azura/
+	else
+		exit
+	fi
+
+}
+
+function unlock () {
+	rm -rf /tmp/lock/azura/
+
+}
 
 function compile () {
-    if [[ $2 ]];then
         sudo cards upgrade && sudo cards install cards.devel
         echo "ok"
-        cd /usr/ports/perso/
-        if [[ $? != 0 ]];then
-            sudo mkdir /usr/ports/perso/
-            echo "Nous venons de créer le répertoire /usr/ports/perso/, nous vous laissons mettre votre pkgfile dans un répertoire possédant le même nom que le paquet à compiler"
+       	cd /usr/ports/perso/${@}
+       	if [[ $? != 0 ]];then
+        	sudo mkdir /usr/ports/perso/
+            	echo "Nous venons de créer le répertoire /usr/ports/perso/, nous vous laissons mettre votre pkgfile dans un répertoire possédant le même nom que le paquet à compiler"
         fi
-        pkgmk -d 
-        pkgadd ${2}1*
-        if [[ $? != 0 ]];then
-            echo "Quelque chose c'est mal passé"
+        sudo pkgmk -d 
+       	sudo pkgadd ${@}1*
+       	if [[ $? != 0 ]];then
+        	echo "Quelque chose c'est mal passé"
         fi
-        echo "Done"
-    fi
+
                 
 }
 
 function update () {
-    echo "Nous mettons votre os à jour"
-    sudo cards upgrade 
-    read -p "Des mises à jour de flatpak sont peut-être disponibles, souhaitez les effectuer ? [o/n] : " flatpak_update_ask
-    if [[ $flatpak_update_ask != "o" ]];then
-        echo "ok"
-    else
-        flatpak update
-    fi
-    sudo cards purge 
+    	echo "Nous mettons votre os à jour"
+    	sudo cards upgrade 
+    	read -p "Des mises à jour de flatpak sont peut-être disponibles, souhaitez les effectuer ? [o/n] : " flatpak_update_ask
+    	if [[ $flatpak_update_ask != "o" ]];then
+        	echo "ok"
+    	else
+        	flatpak update
+    	fi
+    	sudo cards purge 
 
 }
 
 function install () {
-    if [[ $4 ]];then
-        read -p "Le paquet est-il un flatpak, si oui veuillez préciser ? [o/n] : " flatpak_ask
-        if [[ $flatpak_ask != "n" ]];then
-            flatpak update
-            flatpak install $flatpak_ask
-        else
-            sudo cards upgrade
-            sudo cards install ${4}
-            sudo cards purge
-        fi 
-    fi
+    	if [[ $2 ]];then
+        	read -p "Le paquet est-il un flatpak, si oui veuillez préciser ? [o/n] : " flatpak_ask
+        	if [[ $flatpak_ask != "n" ]];then
+            	flatpak update
+            	flatpak install $flatpak_ask
+        	else
+            	sudo cards upgrade
+            	sudo cards install ${2}
+            	sudo cards purge
+        	fi 
+	else
+		echo "utilisez un argument"
+	fi
 }
 
 function remove () {
-    if [[ $6 ]];then
-        read -p "Ces paquets sont-ils des flatpak ? [o/n] : " flatpak_remove_ask
-        if [[ $flatpak_remove_ask != "n" ]];then
-            sudo cards remove $6
-        else
-            flatpak remove $6
-        fi
-        sudo cards purge
-    fi
+    	if [[ $2 ]];then
+        	read -p "Ces paquets sont-ils des flatpak ? [o/n] : " flatpak_remove_ask
+        	if [[ $flatpak_remove_ask != "n" ]];then
+        		sudo cards remove $2
+        	else
+            		flatpak remove $2
+        	fi
+		sudo cards purge
+	else
+		echo "utiliser un argument"
+
+	fi
 }
 
-function main () {
-    # Les arguments 
-    if [[ $1 = "compile" ]];then
-        compile
-    fi
-    if [[ $3 = "install" ]];then
-        install
-    fi
+function main () { 
 
-    if [[ $5 = "update" ]];then
-        update
-    fi
+    	if [[ $1 = "compile" ]];then
+		lock
+		shift
+		compile ${@}
+		unlock
+    	fi
 
-    if [[ $7 = "remove" ]];then
-        remove
-    fi
+    	if [[ $1 = "install" ]];then
+		shift 
+        	install ${@}
+    	fi
+
+    	if [[ $1 = "update" ]];then
+        	update
+    	fi
+
+    	if [[ $1 = "remove" ]];then
+		shift
+        	remove ${@}
+    	fi
 }
+main "$@"
 
-
-
-# Thanks to all the contributors
